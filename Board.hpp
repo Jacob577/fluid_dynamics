9bab4fcc55d961c82ede190fdf8d39c4f83ee344
Jacob577 <deltora.ercisson@gmail.com>
2021-07-26 15:24:14 +0200

    working now?

--- Board.hpp
+++ Board.hpp
@@ -7,15 +7,16 @@ using namespace std;
 class Particle{
   int xVel, yVel;
   int xPos, yPos;
+  int yAcc;
+  bool active = false;
+
+  // public:
+  // vector<float> particleMethod (float xDirection, float yDirection); 
 
   public:
-  vector<float> particleMethod (float xDirection, float yDirection); 
-
-  public:
-  float matrix (float aNumber); 
-
-  public:
-  void myMethod (string thisIsWhatToPrint); 
+  void setyAcc (int acc){
+    yAcc = acc;
+  }
 
   public:
   int getxVel (){
@@ -33,7 +34,10 @@ class Particle{
   int getyPos (){
     return yPos;
   }
-
+  public:
+  bool getActive(){
+    return active;
+  }
   public:
   void setxVel (int xVel1){
     xVel = xVel1;
@@ -49,5 +53,78 @@ class Particle{
   public:
   void setyPos (int yPos1){
     yPos = yPos1;
+  }
+  public:
+  void setActive(bool a){
+    active = a;
+  }
+};
+
+class Board{
+  public:
+  int width, height;
+  vector<vector<Particle>> boardObject;
+
+  public:
+  Particle particleObject; //vector<vector<Particle>>
+
+  void getNewBoard(int w, int h){
+  width = w;
+  height = h;
+  vector<Particle> cols (height, particleObject);
+  vector<vector<Particle>> emptyBoard (width, cols);
+  boardObject = emptyBoard;
+  }
+
+  public:
+  void setParticle(int xcoor, int ycoor, bool active, int xvel, int yvel){
+    boardObject[xcoor][ycoor].setActive(active);
+    boardObject[xcoor][ycoor].setxVel(xvel);
+    boardObject[xcoor][ycoor].setyVel(yvel);
+  }
+  
+
+  public:
+  void updateBoard(){
+    vector<vector<Particle>> temporaryBoard = boardObject;     
+
+    for (int i = 0; i < temporaryBoard[0].size(); i++){
+      for (int j = 0; j < temporaryBoard.size(); j++){
+        //Create the cases for when the relevant particle is by the edges
+        //Check bottom
+        // if (boardObject[i][j].getyPos() + 1 == height){
+        //   temporaryBoard[i][j].setyVel(0);
+        //   boardObject[i][j].setyVel(0);
+          // std::cout << j;
+        // }
+
+        //move down
+        if (boardObject[i][j].getActive()){
+          temporaryBoard[i][j].setActive(false);
+          temporaryBoard[i][j+boardObject[i][j].getyVel()].setActive(true);
+          temporaryBoard[i][j+boardObject[i][j].getyVel()].setyVel(boardObject[i][j].getyVel());
+          temporaryBoard[i][j+boardObject[i][j].getyVel()].setyPos(j);
+        }     
+      }
+    }
+    boardObject = temporaryBoard;   
+  }
+
+  public:
+  void displayBoard (){
+    system("CLS");
+    string displayibleString = "";
+
+    for (int i = 0; i < boardObject[0].size(); i++){
+      for (int j = 0; j < boardObject.size(); j++){
+        if (boardObject[j][i].getActive()){
+          displayibleString.append("#");
+        } else {
+          displayibleString.append("|");
+        }
+      }
+      displayibleString.append("\n");
+    }
+    std::cout << displayibleString;
   }
 };
--- main.cpp
+++ main.cpp
@@ -4,61 +4,60 @@
 #include "Board.hpp"
 using namespace std;
 
-// class Board {
-//   Particle Board;
-
-  // public:
-  // void vector<vector<<Particle>> (Particle)
-// };
-
-
 
 int main() {
-  Particle myPart;
-  int matrixSize = 30;
-  myPart.setxVel(1);
-  myPart.setxPos(0);
-  myPart.setyVel(1);
-  myPart.
-  
-  
-  setyPos(0);
+  boardPointer = new Board;
+  Board board*;
+  // Boar
+  // board.setWidthHeight(25,26);
+  board.getNewBoard(40,20);
+  // board.setParticle(1, 16, true, 1, 1);
+  // board.setParticle(3, 11, true, 1, 1);
+  board.setParticle(3, 1, true, 1, 1);
+  // board.displayBoard();
 
-  std::vector<Particle> matrix (matrixSize, myPart);
-  std::vector<vector<Particle>> matrix2 (matrixSize*2, matrix);
+for (int i = 0; i < 35; i++){
+  board.displayBoard();
+  board.updateBoard();
+  Sleep(500);
+}
 
-  std::vector<string> print1 (matrixSize, " ");
-  std::vector<vector<string>> print2 (matrixSize*2, print1);
-  string myString;
-  bool isInRow = false;
 
-  for (int k = 0; k < 35; k++){
-    for (int i = 0; i < matrixSize; i++){
-      std::cout << endl;
-      for (int j = 0; j < matrixSize*2; j++){
-        isInRow = true;
-        // std::cout << "h ";
-        if (myPart.getxPos() == j){
-          if (myPart.getyPos() == i){
-            // matrix2[i][j] = "#";
-            isInRow = false;
-            myString.append("X");
-          }
-        } 
-        if (isInRow){
-          myString.append(" ");
-        }
-      }
-      myString.append("\n");    
-  }
-    myPart.setxPos(myPart.getxPos() + myPart.getxVel());
-    myPart.setyPos(myPart.getyPos() + myPart.getyVel());
+  // std::vector<Particle> matrix (matrixSize, myPart);
+  // std::vector<vector<Particle>> matrix2 (matrixSize, matrix);
+
+  // std::vector<string> print1 (matrixSize, " ");
+  // std::vector<vector<string>> print2 (matrixSize*2, print1);
+  // string myString;
+  // bool isInRow = false;
+
+  // for (int k = 0; k < 35; k++){
+  //   for (int i = 0; i < matrixSize; i++){
+  //     std::cout << endl;
+  //     for (int j = 0; j < matrixSize*2; j++){
+  //       isInRow = true;
+  //       // std::cout << "h ";
+  //       if (myPart.getxPos() == j){
+  //         if (myPart.getyPos() == i){
+  //           // matrix2[i][j] = "#";
+  //           isInRow = false;
+  //           myString.append("X");
+  //         }
+  //       } 
+  //       if (isInRow){
+  //         myString.append("|");
+  //       }
+  //     }
+  //     myString.append("\n");    
+  // }
+  //   myPart.setxPos(myPart.getxPos() + myPart.getxVel());
+  //   myPart.setyPos(myPart.getyPos() + myPart.getyVel());
     
-    system("CLS");
-    std::cout << myString;
-    myString = "";
-    Sleep(300);    
-  }
+  //   system("CLS");
+  //   std::cout << myString;
+  //   myString = "";
+  //   Sleep(300);    
+  // }
     
   
   // for (int j = 0; j < matrix.size(); j++){
